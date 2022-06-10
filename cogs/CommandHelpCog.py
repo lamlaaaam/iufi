f66e36d668925353432fa9eb106abf65f3a57c8a
lamlaaaam <lamzhiyuan98@gmail.com>
2022-06-10 20:19:20 +0800

    delete qch fail after 2s

--- cogs/CommandHelpCog.py
+++ cogs/CommandHelpCog.py
@@ -10,7 +10,7 @@ class CommandHelpCog(commands.Cog):
     async def commandhelp(self, ctx, cmd):
         cmd = self.bot.get_command(cmd[1:] if cmd[0] in ['q', 'Q'] else cmd)
         if cmd == None:
-            await ctx.send(f"**{ctx.author.mention} The command provided does not exist.**")
+            await ctx.send(f"**{ctx.author.mention} The command provided does not exist.**", delete_after=2)
             return
 
         name               = 'q' + cmd.name

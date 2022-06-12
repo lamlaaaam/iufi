b255e19e90796eac752c508c047cd148249b79b3
lamlaaaam <lamzhiyuan98@gmail.com>
2022-06-12 13:22:07 +0800

    sort collection

--- cogs/CollectionCog.py
+++ cogs/CollectionCog.py
@@ -2,8 +2,7 @@ import asyncio
 import db_utils
 import discord
 from   discord.ext import commands
-from   discord import Button, ButtonStyle
-from   async_timeout import timeout
+from   discord import Button, ButtonStyle, SelectMenu, SelectOption
 
 class CollectionCog(commands.Cog):
     def __init__(self,
@@ -11,6 +10,7 @@ class CollectionCog(commands.Cog):
                  collection_time):
         self.bot             = bot
         self.collection_time = collection_time
+        self.msg_to_event    = {}
 
     async def get_nav_buttons(self, num_pages):
         nav_buttons = [Button(label = '⏮',             custom_id = 'nav_first', style = ButtonStyle.blurple),
@@ -62,37 +62,111 @@ class CollectionCog(commands.Cog):
         desc = desc_pre + await self.page_to_str(pages, 0, cards_per_page)
         embed = discord.Embed(title = title, description = desc, color = discord.Color.gold())
         embed.set_thumbnail(url=user.avatar_url)
-        nav_buttons = [await self.get_nav_buttons(num_pages)]
+        nav_buttons = await self.get_nav_buttons(num_pages)
+        select_menu = [
+            SelectMenu(custom_id='select_menu', options=[
+               SelectOption(emoji='⌛', label='Sort by oldest card first', value='0'),
+               SelectOption(emoji='⌛', label='Sort by latest card first', value='1'),
+               SelectOption(emoji='🆔', label='Sort by ascending card ID', value='2'),
+               SelectOption(emoji='🆔', label='Sort by descending card ID',value='3'),
+               SelectOption(emoji='🏷️', label='Sort by ascending tag name', value='4'),
+               SelectOption(emoji='🏷️', label='Sort by descending tag name', value='5'),
+               SelectOption(emoji='🖼️', label='Sort by ascending frame ID', value='6'),
+               SelectOption(emoji='🖼️', label='Sort by descending frame ID', value='7'),
+               SelectOption(emoji='⭐', label='Sort by ascending star level', value='8'),
+               SelectOption(emoji='⭐', label='Sort by descending star level', value='9'),
+               SelectOption(emoji='👑', label='Sort by ascending rarity', value='10'),
+               SelectOption(emoji='👑', label='Sort by descending rarity', value='11')
+               ],
+            placeholder='Sort collection by ...')
+         ]
 
-        page_msg = await ctx.send(embed=embed, components=nav_buttons)
-
-        def check(i: discord.ComponentInteraction, b):
-            return i.member == ctx.author and i.message.id == page_msg.id
+        page_msg = await ctx.send(embed=embed, components=[nav_buttons, select_menu])
+        self.msg_to_event[page_msg.id] = None
 
         current_page = 0
 
-        try:
-            async with timeout(self.collection_time):
-                while True:
-                    interaction, button = await self.bot.wait_for('button_click', check = check)
-                    cid = button.custom_id
-                    if cid == 'nav_first':
-                        current_page = 0
-                    elif cid == 'nav_left':
-                        current_page = 0 if current_page <= 0 else current_page-1
-                    elif cid == 'nav_right':
-                        current_page = num_pages-1 if current_page >= num_pages-1 else current_page+1
-                    else:
-                        current_page = num_pages-1
-                    nav_buttons[0][2].label = f'{current_page+1}/{num_pages}'
+        async def handle_button_event(user_id, cid):
+            nonlocal current_page
+            if user_id != ctx.author.id:
+                return
+            if cid == 'nav_first':
+                current_page = 0
+            elif cid == 'nav_left':
+                current_page = 0 if current_page <= 0 else current_page-1
+            elif cid == 'nav_right':
+                current_page = num_pages-1 if current_page >= num_pages-1 else current_page+1
+            else:
+                current_page = num_pages-1
+            nav_buttons[2].label = f'{current_page+1}/{num_pages}'
 
-                    desc  = desc_pre + await self.page_to_str(pages, current_page, cards_per_page)
-                    embed = discord.Embed(title = title, description = desc, color = discord.Color.gold())
-                    embed.set_thumbnail(url=user.avatar_url)
-                    await interaction.message.edit(embed=embed, components=nav_buttons)
+            desc  = desc_pre + await self.page_to_str(pages, current_page, cards_per_page)
+            embed.description = desc
+            await page_msg.edit(embed=embed, components=[nav_buttons, select_menu])
 
-        except asyncio.exceptions.TimeoutError:
-            pass
+        async def handle_select_event(user_id, sid):
+            nonlocal current_page, pages
+            if user_id != ctx.author.id:
+                return
+            current_page = 0
+            nav_buttons[2].label = f'1/{num_pages}'
+            new_order = cards_docs_sorted
+            if sid == 0:
+                new_order = cards_docs_sorted
+            if sid == 1:
+                new_order = list(reversed(cards_docs_sorted))
+            if sid == 2:
+                new_order = sorted(cards_docs_sorted, key=lambda d: d['id'])
+            if sid == 3:
+                new_order = sorted(cards_docs_sorted, key=lambda d: d['id'], reverse=True)
+            if sid == 4:
+                new_order = sorted(cards_docs_sorted, key=lambda d: d['tag'] if d['tag'] else "")
+            if sid == 5:
+                new_order = sorted(cards_docs_sorted, key=lambda d: d['tag'] if d['tag'] else "", reverse=True)
+            if sid == 6:
+                new_order = sorted(cards_docs_sorted, key=lambda d: d['frame'])
+            if sid == 7:
+                new_order = sorted(cards_docs_sorted, key=lambda d: d['frame'], reverse=True)
+            if sid == 8:
+                new_order = sorted(cards_docs_sorted, key=lambda d: d['stars'])
+            if sid == 9:
+                new_order = sorted(cards_docs_sorted, key=lambda d: d['stars'], reverse=True)
+            if sid == 10:
+                new_order = sorted(cards_docs_sorted, key=lambda d: d['rarity'])
+            if sid == 11:
+                new_order = sorted(cards_docs_sorted, key=lambda d: d['rarity'], reverse=True)
+            pages = [new_order[i:i + cards_per_page] for i in range(0, collection_size, cards_per_page)]
+            desc  = desc_pre + await self.page_to_str(pages, 0, cards_per_page)
+            embed.description = desc
+            await page_msg.edit(embed=embed, components=[nav_buttons, select_menu])
+            
 
+        interval = 1
+        for _ in range(self.collection_time // interval):
+            await asyncio.sleep(interval)
+            event = self.msg_to_event[page_msg.id]
+            if event:
+                user_id, com = event
+                if isinstance(com, discord.Button):
+                    await handle_button_event(user_id, com.custom_id)
+                else:
+                    await handle_select_event(user_id, com.values[0])
+                self.msg_to_event[page_msg.id] = None
+
+        self.msg_to_event.pop(page_msg.id, None)
         await page_msg.delete()
+        
+    @commands.Cog.listener()
+    async def on_button_click(self, i, b):
+        if i.message.id not in self.msg_to_event:
+            return
+        event = (i.author.id, b)
+        self.msg_to_event[i.message.id] = event
+
+    @commands.Cog.listener()
+    async def on_selection_select(self, i, s):
+        if i.message.id not in self.msg_to_event:
+            return
+        event = (i.author.id, s)
+        self.msg_to_event[i.message.id] = event
         
